bfb68e91022080ba19ba7b6f28459d9027b8686e
Hayden Linville <hayden.linville@gmail.com>
2022-03-31 13:47:55 -0400

    added Note Controller with methods to get and post

--- ElevenNote.Models/NoteListItem.cs
+++ ElevenNote.Models/NoteListItem.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ElevenNote.Models
 {
-    internal class NoteListItem
+    public class NoteListItem
     {
+
+        public int NoteId { get; set; }
+        public string Title { get; set; }
+
+        [Display(Name= "Created")]
+        public DateTimeOffset CreateUtc { get; set; }
     }
 }
